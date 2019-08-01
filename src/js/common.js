46db28ff76a608c4f5ce3c44346793efd4bdfc64
sergeeva104999 <shiny777@bk.ru>
2019-08-01 14:00:11 +0500

    add sass

---
+++ src/js/common.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Minimal React Webpack Babel Setup';
+
+ReactDOM.render(
+<div><h3>{title}</h3></div>,
+document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,100 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
 module.exports = {
-	entry: './src/index.js',
+	entry: {
+		app: './src/index.js'
+	},
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		publicPath: '/dist',
+		filename: '[name].js'
+	},
 	module: {
 		rules: [
 			{
 				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: ['babel-loader']
-			}
+				exclude: '/node_modules/',
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.sass$/,
+				use: [
+					'style-loader',
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							//   // you can specify a publicPath here
+							//   // by default it uses publicPath in webpackOptions.output
+							//publicPath: '/dist/css',
+							//   hmr: process.env.NODE_ENV === 'development',
+						},
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true
+						}
+					}, {
+						loader: 'postcss-loader',
+						options: {
+							sourceMap: true, config: {path: 'src/js/postcss.config.js'}
+						}
+					}, {
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true, 
+							implementation: require("sass")
+						}
+					}
+
+				],
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							//   // you can specify a publicPath here
+							//   // by default it uses publicPath in webpackOptions.output
+							//publicPath: '/dist/css',
+							//   hmr: process.env.NODE_ENV === 'development',
+						},
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true
+						}
+					}, {
+						loader: 'postcss-loader',
+						options: {
+							sourceMap: true, config: {path: 'src/js/postcss.config.js'}
+						}
+					}
+
+				],
+			},
+			
+
 		]
 	},
 	resolve: {
 		extensions: ['*', '.js', '.jsx']
 	},
-	output: {
-		path: __dirname + 'dist',
-		publicPath: '/',
-		filename: 'bundle.js'
-	},
+
 	devServer: {
-		contentBase: './dist'
-	}
+		overlay: true
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			// Options similar to the same options in webpackOptions.output
+			// all options are optional
+			publicPath: './sass/',
+			filename: './css/[name].css'
+		}),
+	],
 };
